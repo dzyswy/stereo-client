374f97860f11d0f55692fc049437b8b63ee1ddc6
dzyswy <857105028@qq.com>
2019-10-25 17:39:13 +0800

    保存

---
+++ stereo_camera/test/rpc_client_demo.cpp
@@ -0,0 +1,47 @@
+#include "stereo_camera.h"
+
+
+
+using namespace std;
+
+
+int main(int argc, char *argv[])
+{
+	int ret;
+	if (argc != )
+	{
+		printf("usage: %s ip cmd_port stream_port\n", argv[0]);
+		return -1;
+	}	
+	
+	string ip = argv[1];
+	int cmd_port = atoi(argv[2]);
+	int stream_port = atoi(argv[3]);
+	
+	stereo_camera cam;
+	
+	ret = cam.open_device(ip.c_str(), cmd_port, stream_port, 2);
+	if (ret < 0) {
+		printf("Failed to open %s cmd:%d stream:%d\n", ip.c_str(), cmd_port, stream_port);
+		return -1;
+	}
+	
+	
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
